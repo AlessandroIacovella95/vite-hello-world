03436d7467ec12eaa81138a12154b2367309b567
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-09-08 10:48:09 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      image: "vue.svg",
+    };
+  },
+
+  methods: {
+    getImageUrl(name) {
+      return new URL(`../assets/${name}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <img :src="getImageUrl(image)" alt="" />
+</template>
+
+<style></style>
